ae0454fb8439cc020e9b0aabb4d8a6d89cb4d635
TWaiLeong <119796060+TWaiLeong@users.noreply.github.com>
2023-04-13 12:44:39 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-2.civmyrxz7yhi.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tengwaileong-employee"
+customregion = "us-east-1"
